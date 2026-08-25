d16f56e35ac60735c15fbbffe262865bd3b4750d
Kalyan Karn <karnkalyan@gmail.com>
2026-08-26 01:45:23 +0545

    fix(server): increase express body parser limit to 50mb to resolve 413 Payload Too Large on imports

--- src/index.js
+++ src/index.js
@@ -64,8 +64,8 @@ const { installConsoleSystemLogger } = require('./utils/systemLogger');
 installConsoleSystemLogger(prisma);
 
 app.use(systemLogger(prisma));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(taskLogger());
 app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
